231859cdcfc56624baf32f788a3f64b80290c1da
Kenneth Solomon <hello@kennethsolomon.com>
2025-04-10 08:01:47 +0800

    add harpoon to my workflow and add dimiss shortcut for noice.lua

--- nvim/lua/kenneth/plugins/noice.lua
+++ nvim/lua/kenneth/plugins/noice.lua
@@ -1,16 +1,19 @@
--- lazy.nvim
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    -- add any options here
+    -- any Noice options go here
   },
+  config = function(_, opts)
+    require("noice").setup(opts)
+
+    -- Keymap to dismiss notifications using the command
+    vim.keymap.set("n", "<leader>nd", function()
+      vim.cmd("NoiceDismiss")
+    end, { desc = "Dismiss Noice notifications" })
+  end,
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-    }
+  },
 }
