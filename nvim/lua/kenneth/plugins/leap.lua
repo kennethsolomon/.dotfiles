21120ec78b28e2cf52650674ba2bcf77f27d0a7b
Kenneth Solomon <hello@kennethsolomon.com>
2025-04-21 19:58:11 +0800

    Add tmux dracula and fix leap colors

--- nvim/lua/kenneth/plugins/leap.lua
+++ nvim/lua/kenneth/plugins/leap.lua
@@ -1,46 +1,70 @@
 return {
-  "ggandor/leap.nvim",
-  config = function()
-    local leap = require("leap")
-    local leap_user = require("leap.user")
+	"ggandor/leap.nvim",
+	config = function()
+		local leap = require("leap")
+		local leap_user = require("leap.user")
 
-    -- Highlight matches
-    leap.opts.highlight_unlabeled_phase_one_targets = true
+		-- Highlight matches
+		leap.opts.highlight_unlabeled_phase_one_targets = true
 
-    -- Limit leap to only the current window
-    leap.opts.safe_labels = {}
+		-- Limit leap to only the current window
+		leap.opts.safe_labels = {}
 
-    -- Case sensitivity
-    leap.opts.case_sensitive = true
+		-- Case sensitivity
+		leap.opts.case_sensitive = false
 
-    -- Disable default `s`, `S`, `gs` mappings
-    leap.set_default_keymaps(false)
+		-- Disable default `s`, `S`, `gs` mappings
+		leap.set_default_keymaps(false)
 
-    -- Safe deletion of keymaps
-    pcall(vim.keymap.del, { 'n', 'x', 'o' }, 's')
-    pcall(vim.keymap.del, { 'n', 'x', 'o' }, 'S')
+		-- Safe deletion of keymaps
+		pcall(vim.keymap.del, { "n", "x", "o" }, "s")
+		pcall(vim.keymap.del, { "n", "x", "o" }, "S")
 
-    -- Set custom mappings
-    vim.keymap.set({ 'n', 'x', 'o' }, 'f', '<Plug>(leap-forward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'F', '<Plug>(leap-backward)')
+		-- Set custom mappings
+		vim.keymap.set({ "n", "x", "o" }, "f", "<Plug>(leap-forward)")
+		vim.keymap.set({ "n", "x", "o" }, "F", "<Plug>(leap-backward)")
 
-    -- Suggested additional tweaks
-    -- Define equivalence classes for brackets and quotes, in addition to
-    -- the default whitespace group:
-    leap.opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+		-- Suggested additional tweaks
+		-- Define equivalence classes for brackets and quotes, in addition to
+		-- the default whitespace group:
+		leap.opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
 
-    -- Use the traversal keys to repeat the previous motion without
-    -- explicitly invoking Leap:
-    leap_user.set_repeat_keys('<enter>', '<backspace>')
+		-- Use the traversal keys to repeat the previous motion without
+		-- explicitly invoking Leap:
+		leap_user.set_repeat_keys("<enter>", "<backspace>")
 
-    -- Define a preview filter (skip the middle of alphanumeric words):
-    leap.opts.preview_filter =
-      function (ch0, ch1, ch2)
-        return not (
-          ch1:match('%s') or
-          ch0:match('%w') and ch1:match('%w') and ch2:match('%w')
-        )
-      end
-  end,
-  lazy = false,
+		-- Define a preview filter (skip the middle of alphanumeric words):
+		leap.opts.preview_filter = function(ch0, ch1, ch2)
+			return not (ch1:match("%s") or ch0:match("%w") and ch1:match("%w") and ch2:match("%w"))
+		end
+
+		-- Leap highlights with Dracula+ palette
+		vim.api.nvim_set_hl(0, "LeapMatch", {
+			fg = "#A4FFFF", -- bright.cyan for targets
+			bg = "NONE",
+			bold = true,
+		})
+
+		vim.api.nvim_set_hl(0, "LeapBackdrop", {
+			fg = "#545454", -- bright.black (subtle)
+		})
+
+		vim.api.nvim_set_hl(0, "LeapLabelPrimary", {
+			fg = "#FF92DF", -- bright.magenta (eye-catching)
+			bg = "NONE",
+			bold = true,
+		})
+
+		vim.api.nvim_set_hl(0, "LeapLabelSecondary", {
+			fg = "#D6ACFF", -- bright.blue for alt targets
+			bg = "NONE",
+		})
+
+		vim.api.nvim_set_hl(0, "LeapLabelSelected", {
+			fg = "#50FA7B", -- green (matches selected match aesthetic)
+			bg = "NONE",
+			bold = true,
+		})
+	end,
+	lazy = false,
 }
