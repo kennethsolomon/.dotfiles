153c70df4c1b4daad6ef4511c69f5d8bb0e3e631
Kenneth Solomon <hello@kennethsolomon.com>
2025-05-29 10:19:12 +0800

    Update worktree settings

--- nvim/lua/kenneth/plugins/git-worktree.lua
+++ nvim/lua/kenneth/plugins/git-worktree.lua
@@ -5,11 +5,10 @@ return {
 
     vim.keymap.set("n", "<leader>gw", function()
       require("telescope").extensions.git_worktree.git_worktrees()
-    end, { desc = "Git Worktree: Create new worktree" })
+    end, { desc = "Git Worktree: List worktrees" })
 
     vim.keymap.set("n", "<leader>gW", function()
       require("telescope").extensions.git_worktree.create_git_worktree()
     end, { desc = "Git Worktree: Create new worktree" })
-
   end,
 }
